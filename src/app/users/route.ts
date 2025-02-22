71abd88a941276d28f574e9a3e3f2a64d2023d84
Lokprakash-babu <lokprakash100@gmail.com>
2025-02-22 13:43:26 +0530

    log removal

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -3,6 +3,5 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   const data = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await data.json();
-  console.log("users", users);
   return NextResponse.json(users);
 };
